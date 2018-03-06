8a02faae1dfab836a49e402fcb721cca1d14b45d
miaochen <miaochen@rogntech.com>
2018-03-06 18:05:00 +0800

    文件上传

--- routers/adminCURD.js
+++ routers/adminCURD.js
@@ -1,29 +1,50 @@
 const router = require('koa-router')();
 let nowStatus = require('../middlewares/check')
 const storeModel = require('../lib/mysql')
+const multer = require('koa-multer');
+var storage = multer.diskStorage({
+    //文件保存路径
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    //修改文件名称
+    filename: function (req, file, cb) {
+        var fileFormat = (file.originalname).split(".");
+        cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);
+    }
+})
+var upload = multer({ storage: storage });
 
-
-router.get('/adm', async (ctx, next) => {//获取所有商品
-    await ctx.render('signup', {
-        session: ctx.session
+router.get('/admstore', async (ctx, next) => {//获取所有商品
+    await storeModel.queryStore().then(res=>{
+        ctx.response.body = res
     })
 })
-router.get('/adm/:id', async (ctx, next) => {//获取指定商品
-    await ctx.render('signup', {
-        session: ctx.session
+router.get('/admstore/:id', async (ctx, next) => {//获取指定商品
+    await storeModel.queryStore(ctx.params.id).then(res=>{
+        if(res&&res.length!==0){
+            ctx.response.body = {code:200,data:res}
+        }else {
+            ctx.response.body = {code:"error",msg:"没找到此编号商品"}
+        }
     })
 })
-router.post('/adm', async (ctx, next) => {//增加商品
-    await ctx.render('signup', {
-        session: ctx.session
+router.post('/admstore', upload.single('file'),async (ctx, next) => {//增加商品
+    await console.log(ctx.req.file)
+    // await storeModel.addStore(ctx.request.body).then(res=>{
+    //     console.log(res)
+    // }).catch(err=>{
+    //     console.log(err.sqlMessage)
+    // })
+})
+router.put('/admstore', async (ctx, next) => {//更新商品
+    await storeModel.updateStore(ctx.request.body).then(res=>{
+        console.log(res)
+    }).catch(err=>{
+        console.log(err.sqlMessage)
     })
 })
-router.put('/adm', async (ctx, next) => {//更新商品
-    await ctx.render('signup', {
-        session: ctx.session
-    })
-})
-router.del('/adm', async (ctx, next) => {//删除商品
+router.del('/admstore', async (ctx, next) => {//删除商品
     await ctx.render('signup', {
         session: ctx.session
     })
