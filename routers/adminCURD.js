ea0cab6e31ace1f12a5bcf006cb086abb50071de
miaochen <328180750@qq.com>
2018-03-06 22:54:45 +0800

    删除商品完成

--- routers/adminCURD.js
+++ routers/adminCURD.js
@@ -10,46 +10,60 @@ var storage = multer.diskStorage({
     //修改文件名称
     filename: function (req, file, cb) {
         var fileFormat = (file.originalname).split(".");
-        cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);
+        cb(null, Date.now() + "." + fileFormat[fileFormat.length - 1]);
     }
 })
-var upload = multer({ storage: storage });
+var upload = multer({storage: storage});
 
 router.get('/admstore', async (ctx, next) => {//获取所有商品
-    await storeModel.queryStore().then(res=>{
+    await storeModel.queryStore().then(res => {
         ctx.response.body = res
     })
 })
 router.get('/admstore/:id', async (ctx, next) => {//获取指定商品
-    await storeModel.queryStore(ctx.params.id).then(res=>{
-        if(res&&res.length!==0){
-            ctx.response.body = {code:200,data:res}
-        }else {
-            ctx.response.body = {code:"error",msg:"没找到此编号商品"}
+    await storeModel.queryStore(ctx.params.id).then(res => {
+        if (res && res.length !== 0) {
+            ctx.response.body = {code: 200, data: res}
+        } else {
+            ctx.response.body = {code: "error", msg: "没找到此编号商品"}
         }
     })
 })
-router.post('/admstore', upload.single('file'),async (ctx, next) => {//增加商品
+router.post('/admstore', upload.single('file'), async (ctx, next) => {//增加商品
     await console.log(ctx.req.file)
-    // await storeModel.addStore(ctx.request.body).then(res=>{
-    //     console.log(res)
-    // }).catch(err=>{
-    //     console.log(err.sqlMessage)
-    // })
+    await storeModel.addStore(ctx.request.body).then(res => {
+        console.log(res)
+    }).catch(err => {
+        console.log(err.sqlMessage)
+    })
 })
 router.put('/admstore', async (ctx, next) => {//更新商品
-    await storeModel.updateStore(ctx.request.body).then(res=>{
+    await storeModel.updateStore(ctx.request.body).then(res => {
         console.log(res)
-    }).catch(err=>{
+        if (res.affectedRows !== 0) {
+            if (res.changedRows !== 0) {
+                ctx.response.body = {code: "ok", msg: "商品更新成功"}
+            } else {
+                ctx.response.body = {code: "error", msg: "数据相同"}
+            }
+
+        } else {
+            ctx.response.body = {code: "error", msg: "商品编码有误，未找到数据"}
+        }
+
+    }).catch(err => {
         console.log(err.sqlMessage)
     })
 })
 router.del('/admstore', async (ctx, next) => {//删除商品
-    await ctx.render('signup', {
-        session: ctx.session
+    await storeModel.deleteStore(ctx.request.body.itemNo).then(res=>{
+        if(res.affectedRows!==0){
+            ctx.response.body = {code:"ok",msg:"删除成功"}
+        }else {
+            ctx.response.body = {code:"error",msg:"没有此编号"}
+        }
+    }).catch(err=>{
+        console.log(err)
     })
 })
-
-
-
 module.exports = router;
